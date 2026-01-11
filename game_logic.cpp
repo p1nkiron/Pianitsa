ec4974bc0a407597970618497186f11289627cc2
iron <nejfeldirina@gmail.com>
2026-01-11 21:46:03 +0500

    Update game_logic.cpp

--- game_logic.cpp
+++ game_logic.cpp
@@ -1,12 +1,9 @@
-﻿#include "card_game.h"
+﻿#include "head.h"
 #include <iostream>
 #include <algorithm>
 #include <random>
 #include <chrono>
-
 using namespace std;
-
-// Инициализация колоды из 36 карт
 void initializeDeck(vector<Card>& deck) {
     deck.clear();
     deck.reserve(DECK_SIZE);
@@ -18,13 +15,11 @@ void initializeDeck(vector<Card>& deck) {
     }
 }
 
-// Перемешивание колоды
 void shuffleDeck(vector<Card>& deck) {
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     shuffle(deck.begin(), deck.end(), default_random_engine(seed));
 }
 
-// Раздача карт двум игрокам
 void dealCards(vector<Card>& deck, vector<Card>& player1, vector<Card>& player2) {
     player1.clear();
     player2.clear();
@@ -38,11 +33,8 @@ void dealCards(vector<Card>& deck, vector<Card>& player1, vector<Card>& player2)
     }
 }
 
-// Преобразование карты в строковое представление
 string cardToString(const Card& card) {
     string result;
-    
-    // Определение достоинства
     switch(card.rank) {
         case 0: result = "6"; break;
         case 1: result = "7"; break;
@@ -55,8 +47,6 @@ string cardToString(const Card& card) {
         case 8: result = "A"; break;
         default: result = "?"; break;
     }
-    
-    // Добавление масти
     switch(card.suit) {
         case 0: result += "-черви"; break;
         case 1: result += "-буби"; break;
@@ -68,104 +58,79 @@ string cardToString(const Card& card) {
     return result;
 }
 
-// Сравнение двух карт
 int compareCards(const Card& card1, const Card& card2) {
-    // Сначала сравниваем достоинства
     if (card1.rank > card2.rank) {
-        return 1; // Первая карта сильнее
+        return 1;
     } else if (card1.rank < card2.rank) {
-        return -1; // Вторая карта сильнее
+        return -1;
     } else {
-        // При равных достоинствах сравниваем масти
-        // В "пьянице" обычно масть не имеет значения, но для полноты реализуем
         if (card1.suit > card2.suit) {
             return 1;
         } else if (card1.suit < card2.suit) {
             return -1;
         }
-        return 0; // Ничья (в теории не должно происходить)
+        return 0;
     }
 }
 
-// Выполнение одного хода
 void playTurn(vector<Card>& player1, vector<Card>& player2, vector<Card>& table, TurnRecord& record, int turn) {
-    
-    // Проверка, что у игроков есть карты
     if (player1.empty() || player2.empty()) {
         return;
     }
-    
-    // Берем верхние карты
+
     Card card1 = player1.front();
     Card card2 = player2.front();
     
-    // Удаляем карты из рук игроков
     player1.erase(player1.begin());
     player2.erase(player2.begin());
-    
-    // Кладем карты на стол
     table.push_back(card1);
     table.push_back(card2);
-    
-    // Заполняем запись о ходе
     record.turn_number = turn;
     record.player1_card = cardToString(card1);
     record.player2_card = cardToString(card2);
     record.player1_cards_count = player1.size();
     record.player2_cards_count = player2.size();
-    
-    // Сравниваем карты
     int result = compareCards(card1, card2);
     
     if (result > 0) {
-        // Первый игрок выиграл ход
         record.winner = "Игрок 1";
-        // Перемещаем все карты со стола в конец колоды первого игрока
         takeAllCards(player1, table);
     } else if (result < 0) {
-        // Второй игрок выиграл ход
         record.winner = "Игрок 2";
-        // Перемещаем все карты со стола в конец колоды второго игрока
         takeAllCards(player2, table);
     } else {
-        // Спор - карты равны, оставляем на столе для следующего хода
         record.winner = "Спор (ничья)";
     }
 }
-
-// Забрать все карты со стола
 void takeAllCards(vector<Card>& winner, vector<Card>& table) {
-    // Перемешиваем карты на столе перед добавлением (по правилам)
     shuffleDeck(table);
-    // Добавляем карты в конец колоды победителя
     winner.insert(winner.end(), table.begin(), table.end());
     table.clear();
 }
 
-// Проверка окончания игры
 bool checkGameEnd(const vector<Card>& player1, const vector<Card>& player2, int turns) {
     if (player1.empty()) {
-        cout << "\n=== Игрок 2 победил! У игрока 1 закончились карты. ===" << endl;
+        cout << "\n Игрок 2 победил! У игрока 1 закончились карты." << endl;
         return true;
     }
     
     if (player2.empty()) {
-        cout << "\n=== Игрок 1 победил! У игрока 2 закончились карты. ===" << endl;
+        cout << "\nИгрок 1 победил! У игрока 2 закончились карты." << endl;
         return true;
     }
     
     if (turns >= MAX_TURNS) {
-        cout << "\n=== Игра завершена по достижении максимального количества ходов (" 
-             << MAX_TURNS << "). ===" << endl;
+        cout << "\n=== Игра завершена по достижении максимального количества ходов (" << MAX_TURNS << ")." << endl;
         if (player1.size() > player2.size()) {
-            cout << "=== Игрок 1 победил по количеству карт! ===" << endl;
+            cout << "Игрок 1 победил по количеству карт!" << endl;
         } else if (player2.size() > player1.size()) {
-            cout << "=== Игрок 2 победил по количеству карт! ===" << endl;
+            cout << "Игрок 2 победил по количеству карт!" << endl;
         } else {
-            cout << "=== Ничья! ===" << endl;
+            cout << "Ничья! " << endl;
         }
         return true;
     }
     
     return false;
+
 }
